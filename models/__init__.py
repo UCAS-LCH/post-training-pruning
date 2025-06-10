02124113a415bd728635513e8015fe2038c74e04
Chenhao LI <31031164+UCAS-LCH@users.noreply.github.com>
2025-06-10 17:57:38 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+from models.resnet import ResNet18, ResNet34, ResNet50, ResNet101
+from models.vgg import vgg11, vgg13, vgg16, vgg19
+
+__all__ = [
+    "ResNet18",
+    "ResNet34",
+    "ResNet50",
+    "ResNet101",
+    "vgg11",
+    "vgg13",
+    "vgg16",
+    "vgg19",
+]
